a10389e2a6a80b7afc36e129a38a8f2e1f24ac85
shonancanuck <shonanxspark@gmail.com>
2023-05-25 13:36:51 +0900

    feat: Setup knexfile and install dotenv

---
+++ backend/knexfile.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: process.env.USER_DB,
+      user: process.env.USER,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+};
