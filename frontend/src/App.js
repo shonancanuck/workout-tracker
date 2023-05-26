7de0485b908809a36e265904ad1efaf3ae2e124c
shonancanuck <shonanxspark@gmail.com>
2023-05-26 18:17:09 +0900

    feat: Initial frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import "./styles/App.css";
+import Days from "./components/Days.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Days />
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Days.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Days() {
+  return (
+    <ul>
+      <li>Sunday</li>
+      <li>Monday</li>
+      <li>Tuesday</li>
+      <li>Wednesday</li>
+      <li>Thursday</li>
+      <li>Friday</li>
+      <li>Saturday</li>
+    </ul>
+  );
+}
